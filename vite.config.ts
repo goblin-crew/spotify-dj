874bf4733b3b4b0c6725bd8816d88bce2deee5a3
Tekknoman <elioteuber@outlook.com>
2024-07-06 01:23:22 +0200

    fix: trying to fix asset path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  experimental: {
+    renderBuiltUrl(filename, { hostType }) {
+      if (hostType === 'js') {
+        return { runtime: `window.__toCdnUrl(${JSON.stringify(filename)})` };
+      } else {
+        return { relative: true };
+      }
+    },
+  },
+});
