7ffc62b5ae7770e6cfdd170241d99f2ce3f3599a
Tekknoman <elioteuber@outlook.com>
2024-07-06 01:33:34 +0200

    fix: adding base to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  experimental: {
-    renderBuiltUrl(filename, { hostType }) {
-      if (hostType === 'js') {
-        return { runtime: `window.__toCdnUrl(${JSON.stringify(filename)})` };
-      } else {
-        return { relative: true };
-      }
-    },
-  },
+  base: '/spotify-dj/develop/',
 });
